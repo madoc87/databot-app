522da739ee3e26a4b788076d89df186746ea5007
madoc87 <madoc87@gmail.com>
2025-02-12 18:06:18 -0300

    Alteraçao do codigo do arquivo api.js para testes de reconhecimento da variavel REACT_APP_API_URL

--- src/api.js
+++ src/api.js
@@ -1,9 +1,19 @@
+// import axios from 'axios';
+
+// const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+
+// const api = axios.create({
+//   baseURL: API_URL,
+// });
+
+// export default api; 
+
+
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
-
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: process.env.REACT_APP_API_URL, 
+  
 });
 
-export default api; 
+export default api;
